912ae7a571a117a8c57d8be39ae35b66adbaa922
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-03 10:41:35 +0200

    [add]: contacts module, contacts in data

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+import contacts from "./contacts.js";
+
+
+console.log(contacts)
+
 const {createApp} = Vue;
 
 createApp({
   data() {
     return {
-
+      contacts
     }
+  },
+
+  mounted() {
+    console.log(this.contacts)
   }
 }).mount('#app')
